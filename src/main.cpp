98e0661a5f97da3f985c06fe9e2e744afc3c1f2e
Carlos Silva <carlosfbsilva8@gmail.com>
2025-10-13 11:49:24 +0100

    add adc read

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
-#include <Arduino.h>
+#include "uart.h"
+#include "adc.h"
+#include <avr/io.h>
+#include <stdio.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int main() {
+  adc_init();
+  uart_init(9600); // Initialize UART with 9600 baud rate
+  uint16_t value;
+  char buffer[10];
+  char info_string[50] = "";
+  
+  while (1) {
+    value = adc_read(0); // Read from ADC channel 0 (e.g., PC0)
+    // Convert ADC value to string and send via UART
+    float voltage = adc_to_voltage(value);
+    float_to_string(voltage, buffer, sizeof(buffer), 2);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+    // Prepare the info string
+    snprintf(info_string, sizeof(info_string), "ADC Value: %s V", buffer);
+    uart_transmit_string(info_string);
+    uart_new_line(); // auto scroll in terminal
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    _delay_ms(500); // Delay for readability
+  }
+  return 0;
 }
